53e3f846b58102341236fc3e7600204535fc8c82
Cody Mitchell <comitche@redhat.com>
2024-05-15 16:51:32 -0400

    update tests for SkeletonTable

--- cypress/component/SkeletonTable.cy.tsx
+++ cypress/component/SkeletonTable.cy.tsx
@@ -2,10 +2,49 @@ import React from 'react';
 import SkeletonTable from '../../packages/module/dist/dynamic/SkeletonTable';
 
 describe('SkeletonTable', () => {
-  it('renders SkeletonTable', () => {
-    const SkeletonTableExample =  <SkeletonTable rowSize={10} columns={[ 'first', 'second' ]} />;
-    cy.mount(SkeletonTableExample);
-    cy.get('table').should('exist');
-    cy.get('table thead tr').should('have.text', 'firstsecond');
+  const columns = [
+    { title: 'First Column', isSortable: true },
+    { title: 'Second Column', isSortable: false },
+  ];
+
+  it('renders SkeletonTable with custom columns', () => {
+    cy.mount(<SkeletonTable columns={columns} rows={5} ouiaId="custom-columns-table" />);
+
+    cy.get('table[data-ouia-component-id="custom-columns-table"]').should('exist');
+    cy.get('thead tr th').should('have.length', 2);
+    cy.get('thead tr th').eq(0).should('have.text', 'First Column');
+    cy.get('thead tr th').eq(1).should('have.text', 'Second Column');
+    cy.get('thead tr th').eq(0).find('.pf-c-table__sort-indicator').should('exist');
+    cy.get('thead tr th').eq(1).find('.pf-c-table__sort-indicator').should('not.exist');
+    cy.get('tbody tr').should('have.length', 5);
   });
-})
+
+  it('renders SkeletonTable with checkboxes', () => {
+    cy.mount(<SkeletonTable columns={columns} rows={3} hasCheckbox={true} ouiaId="checkbox-table" />);
+
+    cy.get('table[data-ouia-component-id="checkbox-table"]').should('exist');
+    cy.get('thead tr th').should('have.length', 3);
+    cy.get('thead tr th[data-ouia-component-id="checkbox-table-th-checkbox"]').should('exist');
+    cy.get('tbody tr').should('have.length', 3);
+    cy.get('tbody tr td[data-ouia-component-id^="checkbox-table-td-"][data-ouia-component-id$="-checkbox"]').should('have.length', 3);
+  });
+
+  it('renders SkeletonTable with expandable rows', () => {
+    cy.mount(<SkeletonTable columns={columns} rows={3} isExpandable={true} ouiaId="expandable-table" />);
+
+    cy.get('table[data-ouia-component-id="expandable-table"]').should('exist');
+    cy.get('thead tr th').should('have.length', 3);
+    cy.get('thead tr th[data-ouia-component-id="expandable-table-th-expandable"]').should('exist');
+    cy.get('tbody tr').should('have.length', 3);
+    cy.get('tbody tr td[data-ouia-component-id^="expandable-table-td-"][data-ouia-component-id$="-expandable"]').should('have.length', 3);
+  });
+
+  it('renders SkeletonTable with sorting', () => {
+    cy.mount(<SkeletonTable columns={columns} rows={5} sortIndex={0} ouiaId="sorted-table" />);
+
+    cy.get('table[data-ouia-component-id="sorted-table"]').should('exist');
+    cy.get('thead tr th').should('have.length', 2);
+    cy.get('thead tr th').eq(0).should('have.class', 'pf-m-selected');
+    cy.get('tbody tr').should('have.length', 5);
+  });
+});
--- packages/module/src/SkeletonTable/SkeletonTable.test.tsx
+++ packages/module/src/SkeletonTable/SkeletonTable.test.tsx
@@ -4,10 +4,38 @@ import SkeletonTable from './SkeletonTable';
 
 describe('SkeletonTable component', () => {
   it('should render correctly', () => {
-    expect(render(<SkeletonTable columns={[ 'first', 'second' ]}/>)).toMatchSnapshot();
+    expect(render(<SkeletonTable columns={[ { title: 'first' }, { title: 'second' } ]} />)).toMatchSnapshot();
   });
 
   it('should render correctly with rows', () => {
-    expect(render(<SkeletonTable columns={[ 'first', 'second' ]} rows={5} />)).toMatchSnapshot();
+    expect(render(<SkeletonTable columns={[ { title: 'first' }, { title: 'second' } ]} rows={5} />)).toMatchSnapshot();
+  });
+
+  it('should render correctly with a caption', () => {
+    expect(
+      render(<SkeletonTable columns={[ { title: 'first' }, { title: 'second' } ]} caption="Test Caption" />)
+    ).toMatchSnapshot();
+  });
+
+  it('should render correctly with a custom OUAI ID', () => {
+    expect(
+      render(<SkeletonTable columns={[ { title: 'first' }, { title: 'second' } ]} ouiaId="customID" />)
+    ).toMatchSnapshot();
+  });
+
+  it('should render correctly with checkboxes', () => {
+    expect(render(<SkeletonTable columns={[ { title: 'first' }, { title: 'second' } ]} hasCheckbox />)).toMatchSnapshot();
+  });
+
+  it('should render correctly with expandable rows', () => {
+    expect(
+      render(<SkeletonTable columns={[ { title: 'first' }, { title: 'second' } ]} isExpandable />)
+    ).toMatchSnapshot();
+  });
+
+  it('should render correctly with a sort index', () => {
+    expect(
+      render(<SkeletonTable columns={[ { title: 'first' }, { title: 'second' } ]} sortIndex={0} />)
+    ).toMatchSnapshot();
   });
 });
